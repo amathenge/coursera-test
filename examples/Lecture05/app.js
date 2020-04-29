1baec4d763996ecb7aef8a72f0412a42ffbc4545
Andrew Mathenge <mathenge@gmail.com>
2020-04-28 23:52:05 -0400

    Commit X

--- examples/Lecture05/app.js
+++ examples/Lecture05/app.js
@@ -4,7 +4,8 @@
 angular.module('myFirstApp', [])
 
 .controller('MyFirstController', function ($scope) {
-  $scope.name = "Yaakov";
+  $scope.name = "Mathenge";
+  $scope.moreData = "This is more data";
   $scope.sayHello = function () {
     return "Hello Coursera!";
   };
--- examples/Lecture06/beginning-of-lecture-code/app.js
+++ examples/Lecture06/beginning-of-lecture-code/app.js
@@ -1,6 +1,25 @@
 (function () {
 'use strict';
 
+angular.module('NameCalculator', [])
+
+.controller('NameCalculatorController', function($scope) {
+  $scope.name = "";
+  $scope.totalValue = 0;
+
+  $scope.displayNumeric = function() {
+    var totalNameValue = calculateNForS($scope.name);
+    $scope.totalValue = totalNameValue;
+  };
+
+  function calculateNForS(s) {
+    var totalSValue = 0;
+    for (var i=0; i < s.length; i++) {
+      totalSValue += s.charCodeAt(i);
+    }
+    return totalSValue;
+  };
+});
 
 
 
